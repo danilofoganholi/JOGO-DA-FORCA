267a0cc7d81c4f5e892b58e1088022d8918c6523
Danilo Foganholi <42190823+danilofoganholi@users.noreply.github.com>
2020-01-03 15:22:54 +0000

    Delete obtemMenuCategoria.kt

--- src/obtemMenuCategoria.kt
+++
@@ -1,7 +0,0 @@
-fun obtemMenuCategoria():String{
-    return "\n>>> Escolha a categoria\n\n" +
-                "A - Frutas\n" +
-                "B - Cidades\n" +
-                "C - Nomes proprios\n" +
-                "0 - Sair sem escolher categoria\n"
-}
