ea8761bc18276b2877a21f8553d8832407181b98
Danilo Foganholi <42190823+danilofoganholi@users.noreply.github.com>
2020-01-03 15:23:16 +0000

    Delete mostraJogo.kt

--- src/mostraJogo.kt
+++
@@ -1,6 +0,0 @@
-fun mostraJogo(categoria:String, numErros:Int, estruturaPalavra:String){
-    //mostra o andamento do jogo
-    println("Categoria: $categoria | Erros: $numErros\n" +
-            "Palavra: $estruturaPalavra\n" +
-            "Introduza uma letra:")
-}
