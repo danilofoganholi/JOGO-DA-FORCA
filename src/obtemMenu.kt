c4b6fed20a80c121f5f8488a9bf4b46011b5eec3
Danilo Foganholi <42190823+danilofoganholi@users.noreply.github.com>
2020-01-03 15:23:01 +0000

    Delete obtemMenu.kt

--- src/obtemMenu.kt
+++
@@ -1,7 +0,0 @@
-fun obtemMenu(): String {
-    //função que mostra o menu
-    return "\n*** JOGO DA FORCA ***\n" +
-                "\n1 - Escolher categoria" +
-                "\n2 - Iniciar jogo" +
-                "\n0 - Sair\n"
-}
