e118fbe5057c187b4009d846b5ff9ffdebd97ede
Danilo Foganholi <42190823+danilofoganholi@users.noreply.github.com>
2020-01-03 15:22:15 +0000

    Delete validaPalpite.kt

--- src/validaPalpite.kt
+++
@@ -1,8 +0,0 @@
-fun validaPalpite(palpite:String): Boolean{
-    return if (palpite.length==1){ // verifica se o palpite tem apenas uma letra
-        when (palpite){
-            in "a".."z",in "A".."Z"-> true // se estiver de 'a' a 'z' retorna true
-            else -> false //caso seja caracteres especiais ou numeros retorna false
-        }
-    }else false //caso tenha mais de uma letra retorna false
-}
